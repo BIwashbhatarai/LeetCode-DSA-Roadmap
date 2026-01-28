9670d4f4488568cc0bb6ba30852ddf77504523d6
Biwash Bhattarai <biwashbhattarai5@gmail.com>
2026-01-28 20:24:59 +0545

    Deleted 2 files

--- String/58_Length_of_Last_Word.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        lastWord = s.split()[-1]
-        return len(lastWord)
