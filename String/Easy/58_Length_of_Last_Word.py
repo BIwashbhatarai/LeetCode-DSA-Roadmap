661a78e2ba50cfe302647d500f60d0c55cf1774f
Biwash Bhattarai <biwashbhattarai5@gmail.com>
2026-01-28 20:24:21 +0545

    Implement lengthOfLastWord using split method

---
+++ String/Easy/58_Length_of_Last_Word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        lastWord = s.split()[-1]
+        return len(lastWord)
