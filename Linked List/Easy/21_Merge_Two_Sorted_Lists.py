525a229c1b0ded82b87cbd71b89b4511a808fcff
Biwash Bhattarai <biwashbhattarai5@gmail.com>
2026-05-26 14:18:14 +0545

    Add solution for LeetCode 21: Merge Two Sorted Lists (without dummy node)

---
+++ Linked List/Easy/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        # check if any list is empty
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        # decide head
+        if list1.val < list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+
+        current = head
+
+        # merge lists
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        # attach remaining nodes
+        current.next = list1 if list1 else list2
+
+        return head
